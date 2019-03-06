f9a429baa626df46d7196b86e4a3c1766bc16ff3
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-03-06 17:11:42 +0200

    Add files via upload

---
+++ ConsoleApp23/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+        public string Address { get; set; }
+        public int Protection { get; set; }
+        public int TotalPurchases { get; set; }
+
+        public Customer(int id, string name, int birthYear, string address, int protection)
+        {
+            Id = id;
+            Name = name;
+            BirthYear = birthYear;
+            Address = address;
+            Protection = protection;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id} Name:{Name} Birth Year:{BirthYear} Protection:{Protection}";
+        }
+    }
+}
---
+++ ConsoleApp23/MyQueue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class MyQueue
+    {
+        private List<Customer> _Customers();
+
+        public MyQueueStack()
+        {
+           
+        }
+        private void Enequeue()
+        {
+
+            _Customers.Insert( Customer);
+        }
+
+    }
+}
